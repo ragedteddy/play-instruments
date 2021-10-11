1ba7ea1f8e40a8110c48a6ea24b7579004e01cec
ragedteddy <55912131+ragedteddy@users.noreply.github.com>
2021-10-11 19:04:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+btns = document.getElementsByClassName("drum");
+
+playlist = {'w':'sounds/crash.mp3',
+            'a':'sounds/kick-bass.mp3',
+            's':'sounds/snare.mp3',
+            'd':'sounds/tom-1.mp3',
+            'j':'sounds/tom-2.mp3',
+            'k':'sounds/tom-3.mp3',
+            'l':'sounds/tom-4.mp3'}
+
+for(var i=0 ; i<btns.length; i++){
+    btns[i].addEventListener("click", function (){
+        var music = new Audio(playlist[this.innerText]);
+        music.play();
+    }) ;
+}
+
+document.addEventListener('keydown', (event) => {
+  var name = event.key;
+  var code = event.code;
+  var music = new Audio(playlist[name]);
+  music.play();
+}, false);
